9b293f5a84f450de751bc1325ddac292788a97b6
Kitsunetic <kitsune.guru@gmail.com>
2020-09-26 20:43:47 +0900

    Update installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='torch_burn',
+      version='0.0.1',
+      url='https://github.com/Kitsunetic/torch_burn',
+      license='MIT',
+      author='Kitsunetic',
+      author_email='1996.jh.shim@gmail.com',
+      description='torch burn',
+      long_description=open('README.md').read(),
+      zip_safe=False,
+      setup_requires=[])
