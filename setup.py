17ec2cbfdf845fb386c7aa6fb37824b24d9fd384
Kitsunetic <kitsune.guru@gmail.com>
2020-10-23 19:53:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='torch_burn',
-      version='0.0.1',
+      version='0.0.2',
       url='https://github.com/Kitsunetic/torch_burn',
       license='MIT',
       author='Kitsunetic',
