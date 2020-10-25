2578d3bb6ba13f4cf7ce6e732d7a917fab64813c
Kitsunetic <kitsune.guru@gmail.com>
2020-10-25 19:14:07 +0900

    Change version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='torch_burn',
-      version='0.0.3',
+      version='0.0.3.2',
       url='https://github.com/Kitsunetic/torch_burn',
       license='MIT',
       author='Kitsunetic',
       author_email='1996.jh.shim@gmail.com',
       description='torch burn',
       long_description=open('README.md').read(),
-      packages=setuptools.find_packages(),
+      packages=find_packages(),
       zip_safe=False,
       setup_requires=[])
